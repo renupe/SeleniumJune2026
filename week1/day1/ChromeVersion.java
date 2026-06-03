52cb3907c2063de1f7b17f01220fd2c7d8847d95
renupe <95033058+renupe@users.noreply.github.com>
2026-06-03 11:49:39 +0530

    Add files via upload

---
+++ week1/day1/ChromeVersion.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class ChromeVersion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Declaration and Initialization
+				float version =91.0f;
+				String val="google";
+				Boolean beta =false;
+				int year=2008;
+				char shortCutKey='c';
+				// printing the values
+				System.out.println(version);
+				System.out.println(val);
+				System.out.println(beta);
+				System.out.println(year);
+				System.out.println(shortCutKey);
+	}
+
+}
